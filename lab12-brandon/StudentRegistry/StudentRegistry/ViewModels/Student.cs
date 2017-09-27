e92c8d21b4b16e6a23374d2698905dc0fad7d599
Brandon Little <brlittle86@gmail.com>
2017-09-27 09:00:35 -0700

    created Student class in ViewModels

---
+++ lab12-brandon/StudentRegistry/StudentRegistry/ViewModels/Student.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentRegistry.ViewModels
+{
+    public class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int Course { get; set; }
+    }
+}
