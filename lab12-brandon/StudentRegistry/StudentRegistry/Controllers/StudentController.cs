427538297d63f6b851766df64ef84caa05e41c06
Brandon Little <brlittle86@gmail.com>
2017-09-27 08:15:35 -0700

    created controller files for Error, Home, and Student

---
+++ lab12-brandon/StudentRegistry/StudentRegistry/Controllers/StudentController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace StudentRegistry.Controllers
+{
+    public class StudentController
+    {
+    }
+}
