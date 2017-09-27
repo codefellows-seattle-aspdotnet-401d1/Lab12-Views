522b15a75f41281aeff9b9b2b707cbfc66d2cfa5
Brandon Little <brlittle86@gmail.com>
2017-09-27 10:07:14 -0700

    removed StudentRegistry class upon being informed that data persistence isn't part of this assignment

--- lab12-brandon/StudentRegistry/StudentRegistry/Controllers/HomeController.cs
+++ lab12-brandon/StudentRegistry/StudentRegistry/Controllers/HomeController.cs
@@ -1,15 +1,16 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace StudentRegistry.Controllers
 {
-    public class HomeController
+    public class HomeController : Controller
     {
-        public string Index()
+        public ViewResult Index()
         {
-            return "Welcome to the Student Registry!";
+            return View();
         }
 
         public string Secret()
