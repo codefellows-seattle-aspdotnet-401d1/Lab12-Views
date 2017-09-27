4c3b64aa25e1509ed9d01bf8cb3bc81d67ff411e
Brandon Little <brlittle86@gmail.com>
2017-09-27 08:21:01 -0700

    created basic HomeController actiosn to build from

--- lab12-brandon/StudentRegistry/StudentRegistry/Controllers/HomeController.cs
+++ lab12-brandon/StudentRegistry/StudentRegistry/Controllers/HomeController.cs
@@ -7,5 +7,14 @@ namespace StudentRegistry.Controllers
 {
     public class HomeController
     {
+        public string Index()
+        {
+            return "Welcome to the Student Registry!";
+        }
+
+        public string Secret()
+        {
+            return "You've found the secret route! Congratulations.";
+        }
     }
 }
