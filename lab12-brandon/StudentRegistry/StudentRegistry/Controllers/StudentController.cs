92c6db334ca945876de77ac9d7773d13ce72c0a1
Brandon Little <brlittle86@gmail.com>
2017-09-27 08:23:37 -0700

    created StudentRegistry class and filled out StudentController

--- lab12-brandon/StudentRegistry/StudentRegistry/Controllers/StudentController.cs
+++ lab12-brandon/StudentRegistry/StudentRegistry/Controllers/StudentController.cs
@@ -7,5 +7,21 @@ namespace StudentRegistry.Controllers
 {
     public class StudentController
     {
+        StudentRegistry registry = new StudentRegistry();
+
+        public string Register(string name, int age, int course)
+        {
+            return registry.RegisterStudent(name, age, course);
+        }
+
+        public string ViewAll()
+        {
+            return registry.ShowStudents();
+        }
+
+        public string Error()
+        {
+            return "Oh noes! An 3rr04!!!!";
+        }
     }
 }
