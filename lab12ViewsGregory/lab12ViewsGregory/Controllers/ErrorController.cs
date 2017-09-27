5916292ce8bb37bb1ea1ded2e3360e75df58bc24
Gregory Dukes <kcils360@gmail.com>
2017-09-27 12:37:38 -0700

    fisrt commit for turn in

---
+++ lab12ViewsGregory/lab12ViewsGregory/Controllers/ErrorController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lab12ViewsGregory.Controllers
+{
+    [Route("Error")]
+
+    public class ErrorController : Controller
+    {
+        [Route("Error")]
+
+        public string Index()
+        {
+            return "Uh oh, something went wrong.";
+        }
+    }
+}
---
+++ lab12ViewsGregory/lab12ViewsGregory/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace lab12ViewsGregory.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+
+        public ViewResult Faculty()
+        {
+            return View();
+        }
+    }
+}
