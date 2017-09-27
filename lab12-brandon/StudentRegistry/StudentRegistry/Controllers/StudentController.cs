145a32f3f00c78ae6b064f4c47045fe6ac7d8452
Brandon Little <brlittle86@gmail.com>
2017-09-27 12:09:17 -0700

    implemented logic and view for the Register() action

--- lab12-brandon/StudentRegistry/StudentRegistry/Controllers/StudentController.cs
+++ lab12-brandon/StudentRegistry/StudentRegistry/Controllers/StudentController.cs
@@ -3,7 +3,6 @@ using StudentRegistry.ViewModels;
 using System.Linq;
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace StudentRegistry.Controllers
