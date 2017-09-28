8522307d19681cd67c64aeef7bb3bb841e7eac43
Austin Cohrs <acohrsdev@outlook.com>
2017-09-27 17:31:50 -0700

    Austin-Lab12

---
+++ Lab12Views/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab12Views.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public string Index()
+        {
+            return "This is my default homepage.\n" +
+                "OPTIONS: \n" +
+                "/welcome \n" +
+                "/students/add?name=____&age=____ \n" +
+                "/students/viewstudents \n" +
+                "/dang";
+        } 
+
+        [Route("welcome")]
+        public string Welcome()
+        {
+            return "This is the Welcome action method.";
+        }
+    }
+}
---
+++ Lab12Views/Views/Students/AddStudent.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Lab12Views.Views.Students
+{
+    public class AddStudentModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
