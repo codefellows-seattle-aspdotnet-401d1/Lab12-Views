ce2f24967fb4ce64e111b00cb20f203f6a1cbadf
Cameron Pam <cammpam.creative@gmail.com>
2017-09-30 18:59:15 -0700

    Lab Complete

---
+++ Lab12Cameron/Lab12Cameron/Controllers/ErrorController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab12Cameron.Controllers
+{
+    public class ErrorController : Controller
+    {
+        [Route("Error")]
+        public ViewResult Index()
+        {
+            string errmsg = "You took a wrong turn somewhere homie!";
+            ViewData["err"] = errmsg;
+            return View();
+
+        }
+
+    }
+}
---
+++ Lab12Cameron/Lab12Cameron/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lab12Cameron.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+        public string MC { get; set; }
+        public string Hometown { get; set; }
+        public int Albums { get; set; }
+        public DateTime FirstAlbum { get; set; }
+    }
+}
